e612860d4ada9f5e8cc7a1cbf6345610b8c615ca
V1niciusK <vskussunoki@gmail.com>
2024-11-14 20:49:53 -0300

    pygame window, close button, and main game loop implemented

--- main.py
+++ main.py
@@ -3,12 +3,32 @@
 # throughout this file
 import pygame
 
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_KINDS, ASTEROID_MIN_RADIUS, ASTEROID_MAX_RADIUS, ASTEROID_SPAWN_RATE
+from constants import *
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    black = pygame.Color(0,0,0)
+
+    # Main Loop
+
+    while True:
+        # Makes close window work
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        
+        # Updates window
+        screen.fill(black)
+        pygame.display.flip()
+
+    """
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    """
+
 
 if __name__ == "__main__":
     main()
